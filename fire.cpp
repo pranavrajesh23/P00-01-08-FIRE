2b40c593cc1015d8278a9a81e4d3501441be0d3b
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-16 22:23:18 +0530

    did safety and seep fire function

--- fire.cpp
+++ fire.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 
 
+void seepInFire()
+{
+    cout<<"\n-------------------------\n";
+    cout<<"FIRE SEEPING IN\n";
+    cout<<"-------------------------\n";
+
+    cout<<"\nDo not open the door if you see fire seeping in.\n";
+    cout<<"\nSeal all cracks with wet towels or clothes.\n";
+    cout<<"\nContact the fire department immediately.\n";
+    cout<<"\nWave at the window to signal for help.\n";
+    cout<<"\nWait for the fire department to arrive.\n";
+}
+
+void safetyMeasures()
+{
+    cout<<"\n-------------------------\n";
+    cout<<"SAFETY MEASURES\n";
+    cout<<"-------------------------\n";
+
+    cout<<"\nBring keys,wallet,phone and wet towel.\n";
+    cout<<"\nDoes the fire seep in through the door?(yes/no)\n";
+    string response;
+    cin>>response;  
+    if(response == "yes")
+    {
+        seepInFire();
+    }
+    else
+    {
+        cout<<"\nLeave the building immediately.\n";
+        downStairsRun();
+    }
+}
 
 void humanDetection()
 {
@@ -16,7 +49,7 @@ void humanDetection()
     if(response == "yes")
     {
         cout<<"\nPush the nearest fire alarm button.\n";
-        // safetyMeasures();
+        safetyMeasures();
     }
     else
     {
@@ -32,7 +65,7 @@ void smokeDetecter()
 
     cout<<"\nSmoke detector has detected the fire.\n";
     cout<<"\nSomke detector triggered the alarm.\n";
-    // safetyMeasures();
+    safetyMeasures();
 }
 
 int main()
